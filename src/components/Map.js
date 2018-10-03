33f87d1dff32ed1e8455c648971421c0410bd38c
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2018-10-03 19:37:41 -0400

    fix: added MapStyle as a json file

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,94 +5,14 @@ import {
   GoogleMap,
   Marker
 } from "react-google-maps";
+import MapStyles from "./MapStyles";
 
 const MyMapComponent = withScriptjs(
   withGoogleMap(props => (
     <GoogleMap
       defaultZoom={13}
       defaultCenter={{ lat: 25.7739436, lng: -80.263992 }}
-      defaultOptions={{
-        styles: [
-          {
-            featureType: "administrative",
-            elementType: "labels.text.fill",
-            stylers: [
-              {
-                color: "#444444"
-              }
-            ]
-          },
-          {
-            featureType: "landscape",
-            elementType: "all",
-            stylers: [
-              {
-                color: "#f2f2f2"
-              }
-            ]
-          },
-          {
-            featureType: "poi",
-            elementType: "all",
-            stylers: [
-              {
-                visibility: "off"
-              }
-            ]
-          },
-          {
-            featureType: "road",
-            elementType: "all",
-            stylers: [
-              {
-                saturation: -100
-              },
-              {
-                lightness: 45
-              }
-            ]
-          },
-          {
-            featureType: "road.highway",
-            elementType: "all",
-            stylers: [
-              {
-                visibility: "simplified"
-              }
-            ]
-          },
-          {
-            featureType: "road.arterial",
-            elementType: "labels.icon",
-            stylers: [
-              {
-                visibility: "off"
-              }
-            ]
-          },
-          {
-            featureType: "transit",
-            elementType: "all",
-            stylers: [
-              {
-                visibility: "off"
-              }
-            ]
-          },
-          {
-            featureType: "water",
-            elementType: "all",
-            stylers: [
-              {
-                color: "#1994c1"
-              },
-              {
-                visibility: "on"
-              }
-            ]
-          }
-        ]
-      }}
+      defaultOptions={{ styles: MapStyles }}
     >
       {props.isMarkerShown && (
         <Marker
