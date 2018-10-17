cccba5d18376a83a7838561e865b83a553c4b3dc
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2018-10-17 15:04:03 -0400

    feat: added tabIndex for a11y

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -10,7 +10,9 @@ class ListItem extends Component {
     return (
       <li
         className="list-item"
+        aria-labelledby="listItem"
         onClick={() => this.props.listItemClick(this.props)}
+        tabIndex={0}
       >
         <p>{this.props.title}</p>
       </li>
