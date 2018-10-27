7aeeea1a11e361527808c5e13411efa3bae33e95
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2018-10-27 00:07:20 -0400

    added styles for hamburger menu

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { elastic as Menu } from "react-burger-menu";
+import SideBar from "./SideBar";
 
 class Hamburger extends Component {
   showSettings(event) {
@@ -10,18 +11,7 @@ class Hamburger extends Component {
     return (
       <div id="outer-container">
         <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"}>
-          <a id="home" className="menu-item" href="/">
-            Home
-          </a>
-          <a id="about" className="menu-item" href="/about">
-            About
-          </a>
-          <a id="contact" className="menu-item" href="/contact">
-            Contact
-          </a>
-          <a onClick={this.showSettings} className="menu-item--small" href="">
-            Settings
-          </a>
+          <SideBar {...this.props} />
         </Menu>
       </div>
     );
