ccac43f6e88507a0a3a270ac394f6c1d1e76986f
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2018-10-29 20:53:40 -0400

    removed hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-import Hamburger from "./Hamburger";
 
 class Header extends Component {
   state = {};
   render() {
     return (
       <header id="header">
-        <Hamburger />
         <h2>Miami Neighborhood Map</h2>
       </header>
     );
